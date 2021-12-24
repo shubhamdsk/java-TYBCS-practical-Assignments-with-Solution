bd331dd7b52dbebee5c4da254532370e402db986
Shubham Kailas Deshmukh <87111500+shubhamdsk@users.noreply.github.com>
2021-12-24 19:48:35 +0530

    Assignment 3

--- Practical Assignment 3/MarkerInterface.java
+++ Practical Assignment 3/MarkerInterface.java
@@ -1,3 +1,7 @@
+
+/*Set B
+b) Write a program to using marker interface create a class product(product_id, product_name, product_cost, product_quantity) define a default and parameterized constructor. Create objects of class product and display the contents of each object and Also display the object count.
+*/
 import java.util.*;
 
 interface MarkerInt {
@@ -26,38 +30,51 @@ class product implements MarkerInt {
         pcost = c;
         quantity = q;
         cnt++;
+        System.out.println("\nCOUNT OF OBJECT IS : " + cnt + "\n");
     }
 
     public void display() {
-        System.out.println("\n Product Details..");
-        System.out.println("Id\tPname\tCost\tQuantity\n");
-        System.out.println(pid + "\t" + pname + "\t" + pcost + "\t" + quantity);
-        
+
+        System.out.println("\t" +pid + "\t" + pname + "\t" + pcost + "\t" + quantity);
+
     }
 }
 
 public class MarkerInterface {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Product ID: ");
-        int pid = sc.nextInt();
 
-        sc.nextLine();
-        System.out.println("Enter Product Name: ");
-        String pn = sc.nextLine();
+        System.out.println("Enter Number of Product : ");
+        int n = sc.nextInt();
 
-        System.out.println("Enter Product Cost:");
-        int pc = sc.nextInt();
+        product pr[] = new product[n];
+        for (int i = 0; i < n; i++) {
 
-        System.out.println("Enter Product Quantity:");
-        int pq = sc.nextInt();
+            System.out.println("\nEnter " + (i + 1) + " Product Details :\n");
 
-        product p1 = new product();
-        p1.display();
+            System.out.println("Enter Product ID: ");
+            int pid = sc.nextInt();
 
-        product p = new product(pid, pn, pc, pq);
-        p.display();
+            sc.nextLine();
+            System.out.println("Enter Product Name: ");
+            String pn = sc.nextLine();
+
+            System.out.println("Enter Product Cost:");
+            int pc = sc.nextInt();
+
+            System.out.println("Enter Product Quantity:");
+            int pq = sc.nextInt();
+
+            pr[i] = new product(pid, pn, pc, pq);
+
+        }
+        System.out.println("\n\t\t Product Details\n");
+        System.out.println("\tId\tPname\tCost\tQuantity\n");
+        for (int i = 0; i < n; i++) {
+            pr[i].display();
+        }
 
         sc.close();
     }
