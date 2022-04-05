30dbaab7c033647c17ba9e62de7c3cbfa3a177bf
Shubham Kailas Deshmukh <87111500+shubhamdsk@users.noreply.github.com>
2022-04-05 10:50:51 +0530

    ResultSetMetaData program of DONOR

---
+++ SII_Assignment1/DONOR.java
@@ -0,0 +1,42 @@
+/**
+ * C].Write a program to display information about all coumns in the DONOR table using 
+/**
+ * @author shubham Deshmukh
+ *
+ */
+import java.sql.*;
+public class DONOR {
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            // Use database name & password according to your "dbname","pass"
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "dsk");
+
+            Statement stmt = null;
+            stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery("select * from donor");
+            ResultSetMetaData rsmd = rs.getMetaData();
+
+            int n = rsmd.getColumnCount();
+
+            for (int i = 1; i < n; i++) {
+                System.out.println("Column No :" + i);
+                System.out.println("Column Name : " + rsmd.getColumnName(i));
+                System.out.println("Column Type : " + rsmd.getColumnTypeName(i));
+                System.out.println("Column Display Size : " + rsmd.getColumnDisplaySize(i));
+
+            } // for
+
+            rs.close();
+            stmt.close();
+            conn.close();
+
+        } // try
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
---
+++ SII_Assignment1/donor.pgsql
@@ -0,0 +1,8 @@
+-- create table donor(did int, dname char(22),daddr varchar(22));
+
+-- insert into donor VALUES(1,'AAA','zzz');
+-- insert into donor VALUES(2,'BBB','yyy');
+-- insert into donor VALUES(3,'CCC','xxx');
+-- insert into donor VALUES(4,'DDD','www');
+
+SELECT * from donor;
