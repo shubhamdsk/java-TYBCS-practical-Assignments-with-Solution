332e75e067dde7632ad46c715bc9b1c8ffec7f7a
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-04-10 21:00:35 +0530

    Fix #1

--- SII_Assignment1/ProjectDisplay.java
+++ SII_Assignment1/ProjectDisplay.java
@@ -3,6 +3,8 @@
  * @author:Shubham deshmukh
  * 
  * a) Create a PROJECT table with fields project_id, Project_name, Project_description, Project_Status. etc. Insert values in the table. Display all the details of the PROJECT table in a tabular format on the screen.(using swing).
+
+ *All functions are covered such as insert,update,display,delete and exit.
  */
 
 import java.sql.*;
