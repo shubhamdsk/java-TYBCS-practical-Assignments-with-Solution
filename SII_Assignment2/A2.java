b7794807ee40ee9af70e308afae009332563d433
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-04-11 21:57:32 +0530

    LinkedList Program

---
+++ SII_Assignment2/A2.java
@@ -0,0 +1,26 @@
+/**
+ * @author:Shubham Deshmukh
+ * 
+ * b) Write a java program to read ‘n’ names of your friends, store it into linked list, also display contents of the same.
+ */
+
+import java.util.*;
+public class A2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        LinkedList ll = new LinkedList();
+
+        System.out.println("Enter How many Friends :");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the "+n+" Friends :");
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String fl = sc.nextLine();
+            ll.add(fl);
+        }
+        System.out.println("Cities :" + ll);
+        sc.close();
+    }
+}
