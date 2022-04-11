23993c22b1eeec24e2946290e274dc25bf967593
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-04-11 22:32:14 +0530

    TreeSet Program

---
+++ SII_Assignment2/A3.java
@@ -0,0 +1,26 @@
+/**
+ * @author:Shubham Deshmukh
+ * 
+ * c) Write a program to create a new tree set, add some colors (string) and print out the tree set.
+ */
+
+import java.util.*;
+public class A3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        TreeSet ts = new TreeSet();
+
+        System.out.println("Enter How many Colours :");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the "+n+" Colours :");
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String c = sc.nextLine();
+            ts.add(c);
+        }
+        System.out.println("Colours :" + ts);
+        sc.close();
+    }
+}
