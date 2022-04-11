3c4395f0b951edaa555a0cd2b03bc6a30cb9f2d7
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-04-11 21:52:08 +0530

    ArrayList Program

---
+++ SII_Assignment2/A1.java
@@ -0,0 +1,26 @@
+/**
+ * @author:Shubham Deshmukh
+ * 
+ * a) Write a java program to accept names of ‘n’ cities, insert same into array list collection and display the contents of same array list, also remove all these elements.
+ */
+
+import java.util.*;
+public class A1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList al = new ArrayList();
+
+        System.out.println("Enter How many cities :");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the Cities :");
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String c = sc.nextLine();
+            al.add(c);
+        }
+        System.out.println("Cities :" + al);
+        sc.close();
+    }
+}
