6f365833dfbdb3dc5f100029998bff424024bd90
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-04-12 19:53:29 +0530

    Collection Programs

---
+++ SII_Assignment2/C2.java
@@ -0,0 +1,34 @@
+/**
+ * @author:Shubham Deshmukh
+ * 
+ * b) Write a program to create link list of integer objects. Do the following: 
+ * i. add element at first position
+ * ii. delete last element 
+ * iii. display the size of link list
+ * */
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class C2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList ll = new LinkedList<>();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+
+        System.out.println("\nElements in List :\n" + ll);
+
+        ll.addFirst(0);
+
+        System.out.println("\nList after adding Elements at First :\n" + ll);
+
+        ll.removeLast();
+
+        System.out.println("\nList after deleting Last Element :\n" + ll);
+
+        System.out.println("\nSize of the List :\n" + ll.size());
+
+        sc.close();
+    }
+}
