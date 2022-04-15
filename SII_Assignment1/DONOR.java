56b7e39a23a5353ac66e4cb19a4ca7e49e77a9c6
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-04-15 18:59:07 +0530

    ResultsetMetaData Program

--- SII_Assignment1/DONOR.java
+++ SII_Assignment1/DONOR.java
@@ -1,46 +1,48 @@
-
 /**
  * @author shubham Deshmukh
  * 
- * C].Write a program to display information about all coumns in the DONOR table using 
- * 
+ * C].Write a program to display information about all coumns in the DONOR table using ResultSetMetaData.
  * 
  *for database material use here donor.pgsql file
 **/
 
 import java.sql.*;
+
 public class DONOR {
     public static void main(String[] args) {
-
         try {
+            // load a driver
             Class.forName("org.postgresql.Driver");
-            // Use database name & password according to your "dbname","pass"
+
+            // Establish Connection
             Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "postgres", "dsk");
 
             Statement stmt = null;
             stmt = conn.createStatement();
-
             ResultSet rs = stmt.executeQuery("select * from donor");
+
             ResultSetMetaData rsmd = rs.getMetaData();
+            System.out.println("\t-------------------------------------------------");
 
-            int n = rsmd.getColumnCount();
-
-            for (int i = 1; i < n; i++) {
-                System.out.println("Column No :" + i);
-                System.out.println("Column Name : " + rsmd.getColumnName(i));
-                System.out.println("Column Type : " + rsmd.getColumnTypeName(i));
-                System.out.println("Column Display Size : " + rsmd.getColumnDisplaySize(i));
-
+            int count = rsmd.getColumnCount();
+            System.out.println("\t No. of Columns: " + rsmd.getColumnCount());
+            System.out.println("\t-------------------------------------------------");
+            for (int i = 1; i <= count; i++) 
+            {
+                System.out.println("\t\tColumn No : " + i);
+                System.out.println("\t\tColumn Name : " + rsmd.getColumnName(i));
+                System.out.println("\t\tColumn Type : " + rsmd.getColumnTypeName(i));
+                System.out.println("\t\tColumn Display Size : " + rsmd.getColumnDisplaySize(i));
+                System.out.println();
             } // for
+            System.out.println("\t--------------------------------------------------");
 
             rs.close();
             stmt.close();
             conn.close();
-
         } // try
         catch (Exception e) {
             System.out.println(e);
-        }
+        } // catch
     }
-
 }
