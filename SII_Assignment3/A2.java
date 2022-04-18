75c8bf1be90dd676f4bf6ea6a603628d043df5ca
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-04-18 21:19:09 +0530

    Thread Program

--- SII_Assignment3/A2.java
+++ SII_Assignment3/A2.java
@@ -1,20 +1,22 @@
-public class A2
-{
-	public static void main(String[] args)
-	{
-		try{
-Thread t = Thread.currentThread();
-t.setName("Reverse Thread");
-System.out.println(t);
-for(int i=100;i>=1;i--)
-{
-System.out.println(i);
-t.sleep(6000);
-}
-		}//try
-		catch(Exception e)
-		{
+/**
+ * 
+ * @author:Shubham Deshmukh
+ * 
+ * b) Write a program in which thread sleep for 6 sec in the loop in reverse order from 100 to 1 and change the name of 	thread.
+ */
+public class A2 {
+	public static void main(String[] args) {
+		try {
+			Thread t = Thread.currentThread();
+			t.setName("Reverse Thread");
+			System.out.println(t);
+			for (int i = 100; i >= 1; i--) {
+				System.out.println(i);
+				Thread.sleep(6000);
+			}
+		} // try
+		catch (Exception e) {
 			System.out.println(e);
-		}//catch
-	}//main
-}//class
+		} // catch
+	}// main
+}// class
