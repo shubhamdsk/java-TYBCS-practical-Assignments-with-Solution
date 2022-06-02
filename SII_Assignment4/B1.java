12a90ec007752ebc604e2d5e111ba1c84fc20abd
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-06-02 17:57:24 +0530

    set b first program

--- SII_Assignment4/B1.java
+++ SII_Assignment4/B1.java
@@ -4,52 +4,46 @@ import javax.servlet.http.*;
 import java.sql.*;
 import java.util.*;
 
+public class B1 extends HttpServlet {
 
-public class B1 extends HttpServlet{
+    public void doGet(HttpServletRequest request, HttpServletResponse responce) throws IOException, ServletException {
 
-public void doGet(HttpServletRequest request,HttpServletResponse responce) throws IOException, ServletException
-{
+        responce.setContentType("text/html");
+        PrintWriter out = responce.getWriter();
 
-responce.setContentType("text/html");
-PrintWriter out= responce.getWriter();
+        try {
 
-try{
+            Scanner sc = new Scanner(System.in);
+            Connection con = null;
+            ;
+            Statement st = null;
+            ResultSet rs = null;
 
-Scanner sc= new Scanner(System.in);
-Connection con=null;;
-Statement st=null;
-ResultSet rs=null;
+            // load driver
+            Class.forName("org.postgresql.Driver");
+            out.println("<h2>Driver load</h2>");
 
-//load driver
-Class.forName("org.postgresql.Driver");
-out.println("<h2>Driver load</h2>");
+            // establish a conn
+            con = DriverManager.getConnection("jdbc:postgresql://localhost/bcs", "postgres", "");
 
-//establish a conn
-con=DriverManager.getConnection("jdbc:postgresql://localhost/bcs","postgres","");
- 
-out.println("<h2>connection load</h2>");
+            out.println("<h2>connection load</h2>");
 
-int cnum=Integer.parseInt(request.getParameter("num"));
+            int cnum = Integer.parseInt(request.getParameter("num"));
 
-st=con.createStatement();
-rs=st.executeQuery("select * from cust where id="+cnum);
+            st = con.createStatement();
+            rs = st.executeQuery("select * from cust where id=" + cnum);
 
-while(rs.next()){
-out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getInt(3));
+            while (rs.next()) {
+                out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getInt(3));
 
-out.println("<br>");
-}
+                out.println("<br>");
+            }
 
+        } catch (Exception e) {
+            out.println(e);
+        }
 
-
-}catch(Exception e){
-out.println(e);
-}
-
-
-
-out.close();
-}
+        out.close();
+    }
 
 }
-
