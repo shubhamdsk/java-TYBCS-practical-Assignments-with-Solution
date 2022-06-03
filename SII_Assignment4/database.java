54c2626c918d9098bdab4c22231909a67ed46e9d
shubham Deshmukh <shubhamdeshm37@gmail.com>
2022-06-03 07:52:04 +0530

    Required Changes

--- SII_Assignment4/database.java
+++ SII_Assignment4/database.java
@@ -7,28 +7,17 @@ import javax.servlet.http.*;
 
 public class database extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse responce) throws IOException, ServletException {
-
         responce.setContentType("text/html");
         PrintWriter out = responce.getWriter();
-
         try {
-
             Scanner sc = new Scanner(System.in);
             Connection con = null;
-            ;
             Statement st = null;
             ResultSet rs = null;
-
             // load driver
             Class.forName("org.postgresql.Driver");
-            out.println("<h2>Driver load</h2>");
-
             // establish a conn
             con = DriverManager.getConnection("jdbc:postgresql://localhost/bcs", "postgres", "");
-
-            out.println("<h2>connection load</h2>");
-
-            int cnum = Integer.parseInt(request.getParameter("num"));
 
             st = con.createStatement();
             rs = st.executeQuery("select * from student ");
@@ -42,8 +31,7 @@ public class database extends HttpServlet {
         } catch (Exception e) {
             out.println(e);
         }
-
         out.close();
-        
+
     }
 }
